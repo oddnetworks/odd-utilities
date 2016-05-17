181f8c9319593811b56cb6da4f5619c233aa85af
Patrick McConnell <mcconnellpatrick@gmail.com>
2016-05-17 13:23:00 -0400

    improves error reporting (needs more work)

--- OddPlayerTest-iOS/OddPlayerTest/ViewController.swift
+++ OddPlayerTest-iOS/OddPlayerTest/ViewController.swift
@@ -10,15 +10,38 @@ import UIKit
 import AVKit
 import AVFoundation
 
+private var myContext = 0
+
+class MyAVPlayerItem: AVPlayerItem {
+  
+  var observer: UIViewController? = nil
+  
+  deinit {
+    if observer != nil {
+      self.removeObserver(observer!, forKeyPath: "status", context: &myContext)
+    }
+  }
+}
+
 class ViewController: UIViewController, UITextFieldDelegate {
 
   @IBOutlet var urlTextField: UITextField!
   @IBOutlet weak var playButton: UIButton!
+
+  var mediaItem: MyAVPlayerItem?
   
   override func viewDidLoad() {
     super.viewDidLoad()
     self.urlTextField.addTarget(self, action: #selector(self.textFieldChanged), forControlEvents: .EditingChanged)
+    
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.newAVPlayerError), name: AVPlayerItemNewErrorLogEntryNotification, object: nil)
   }
+  
+  override func viewWillDisappear(animated: Bool) {
+    super.viewWillDisappear(animated)
+//    mediaItem?.removeObserver(self, forKeyPath: "status", context: &myContext)
+  }
+  
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
@@ -38,15 +61,62 @@ class ViewController: UIViewController, UITextFieldDelegate {
     case .Some("playVideoSegue"):
       guard let vc = segue.destinationViewController as? AVPlayerViewController,
       urlString = self.urlTextField.text,
-      videoURL = NSURL(string: urlString) else { break }
+      videoURL = NSURL(string: urlString) else {
+        showAlert("Unable to create url")
+        break
+      }
       
-      let mediaItem = AVPlayerItem(URL: videoURL)
-      let player = AVPlayer(playerItem: mediaItem)
+      mediaItem = MyAVPlayerItem(URL: videoURL)
+      
+      if mediaItem == nil {
+        showAlert("Unable to create mediaItem")
+        break
+      }
+      
+      mediaItem!.observer = self
+      mediaItem!.addObserver(self, forKeyPath: "status", options: .New, context: &myContext)
+      let player = AVPlayer(playerItem: mediaItem!)
       vc.player = player
       vc.player?.play()
     default: break
     }
   }
-  
-}
 
+  override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+    if context == &myContext {
+      if keyPath == "status" {
+        guard let theChange = change,
+          statusInt = theChange[NSKeyValueChangeNewKey] as? Int,
+          status = AVPlayerItemStatus(rawValue: statusInt) else { return }
+        if status == .Failed {
+          showAlert("Error playing video")
+        }
+      }
+    }
+  }
+  
+  func showAlert(message: String) {
+    let alert = UIAlertController(title: "Error", message: message, preferredStyle: .Alert)
+    
+    let ok = UIAlertAction(title: "OK", style: .Default, handler: nil)
+    alert.addAction(ok)
+  
+    guard let item = self.mediaItem,
+      log = item.errorLog() else {
+        self.view.window?.rootViewController?.presentViewController(alert, animated: true, completion: {})
+        return
+    }
+    
+    let showLog = UIAlertAction(title: "Show Error Log", style: .Default) { (action) in
+      print(log)
+    }
+        
+    alert.addAction(showLog)
+    
+    self.view.window?.rootViewController?.presentViewController(alert, animated: true, completion: {})
+  }
+  
+  func newAVPlayerError() {
+    print("Error")
+  }
+}
