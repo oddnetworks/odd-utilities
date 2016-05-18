e0e8cb63461d9f9a05403d48b8226659488a1061
Patrick McConnell <mcconnellpatrick@gmail.com>
2016-05-18 11:47:49 -0400

    initial exo player

--- OddPlayerTest-Android/app/src/main/java/oddnetworks/io/oddplayertest/MainActivity.java
+++ OddPlayerTest-Android/app/src/main/java/oddnetworks/io/oddplayertest/MainActivity.java
@@ -1,6 +1,7 @@
 package oddnetworks.io.oddplayertest;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
@@ -9,10 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.exoplayer.util.Util;
 
+public class MainActivity extends AppCompatActivity {
     private EditText urlEditText;
     private Button playButton;
+    private Button exoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +23,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         configureTextListener();
-        configurePlayButtonClickHandler();
+        configurePlayButtonClickHandlers();
     }
 
     void configureTextListener() {
         urlEditText = (EditText) findViewById(R.id.url_editText);
         playButton = (Button) findViewById(R.id.play_button);
+
+        urlEditText.setText("http://www.sample-videos.com/video/mp4/240/big_buck_bunny_240p_1mb.mp4", null);
 
         urlEditText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -41,27 +46,48 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
                 playButton.setEnabled(s.length() > 0);
+                exoButton.setEnabled(s.length() > 0);
             }
         });
     }
 
-    void configurePlayButtonClickHandler() {
-        Button playButton = (Button) findViewById(R.id.play_button);
+    void configurePlayButtonClickHandlers() {
+        playButton = (Button) findViewById(R.id.play_button);
         if (playButton != null) {
             playButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    switchToPlayerView();
+                    switchToPlayerView(PlayerType.NATIVE);
+                }
+            });
+        }
+
+        exoButton = (Button) findViewById(R.id.exo_button);
+        if (exoButton != null) {
+            exoButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switchToPlayerView(PlayerType.EXO);
                 }
             });
         }
     }
 
-    void switchToPlayerView() {
-        Intent videoPlayerIntent = new Intent(MainActivity.this, VideoPlayerActivity.class);
+    void switchToPlayerView(PlayerType playerType) {
+        Intent videoPlayerIntent;
+        videoPlayerIntent = new Intent(MainActivity.this, VideoPlayerActivity.class);
         String url = urlEditText.getText().toString();
 
-        videoPlayerIntent.putExtra("url", url);
+        if (playerType == PlayerType.NATIVE) {
+            videoPlayerIntent.putExtra("url", url);
+        } else {
+            videoPlayerIntent = new Intent(MainActivity.this, ExoPlayerActivity.class)
+                .setData(Uri.parse(url))
+                .putExtra(ExoPlayerActivity.CONTENT_ID_EXTRA, "contentId")
+                .putExtra(ExoPlayerActivity.CONTENT_TYPE_EXTRA, Util.TYPE_OTHER)
+                .putExtra(ExoPlayerActivity.PROVIDER_EXTRA, "provider");
+        }
+
         MainActivity.this.startActivity(videoPlayerIntent);
     }
 }
---
+++ OddPlayerTest-Android/app/src/main/java/oddnetworks/io/oddplayertest/PlayerType.java
@@ -0,0 +1,9 @@
+package oddnetworks.io.oddplayertest;
+
+/**
+ * Created by patrickmcconnell on 5/17/16.
+ */
+public enum PlayerType {
+    NATIVE,
+    EXO
+}
